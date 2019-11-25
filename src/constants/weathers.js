d7a70765a134987ec33f0921b4a67c8ded17610b
Jasiel <macbook@Macbooks-MacBook-Pro.local>
2019-11-25 01:34:57 -0800

    cinstantes

---
+++ src/constants/weathers.js
@@ -0,0 +1,16 @@
+export const CLOUD = "cloud";
+
+export const CLOUDY = "cloudy";
+
+export const SUN = "sun";
+
+export const RAIN = "rain";
+
+export const SNOW = "snow";
+
+export const WINDY = "windy";
+
+
+
+
+
